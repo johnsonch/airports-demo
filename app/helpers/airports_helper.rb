9873f8ffc95c78f24e923fcb54cb49ec9d73e4bf
Chris Johnson <chris@johnsonch.com>
2014-11-06 16:39:49 +0000

    more setup for adding weather API

--- app/helpers/airports_helper.rb
+++ app/helpers/airports_helper.rb
@@ -1,2 +1,11 @@
 module AirportsHelper
+  def kelvin_to_fahrenheit(k)
+    ((k - 273.15)* 1.8000 + 32.00).round(0)
+  end
+  
+  def compass_point(bearing)
+    points = %w[N NE E SE S SW W NW]
+    seg_size = 360 / points.size
+    points[((bearing + (seg_size / 2)) % 360) / seg_size]
+  end
 end
